cf52a339ecef96a8fdb62606bdda5f33c7429c7e
MD RAJ <rajmd1800@gmail.com>
2024-08-30 15:25:15 +0530

    java script learning started

---
+++ JS Learning/01var.js
@@ -0,0 +1,16 @@
+const accountId= 12312;
+let accountEmail = "demo@google.com";
+var accountPassword = "1223235";
+accountCity ="jaipur";
+let accountState;
+let temp =null;
+
+
+console.log(accountId);
+console.table([
+    accountId,
+    accountEmail,
+    accountPassword,
+    accountCity,
+    accountState,
+]);
